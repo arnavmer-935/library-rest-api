2b8753902c817fb20aae0691f77fd893ef9b1557
Arnav <arniemerani@gmail.com>
2026-08-04 01:36:59 +0530

    add missing env configuration to server

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import app from "./app.js";
+import dotenv from "dotenv";
 
-const PORT = process.env.APP_PORT;
+dotenv.config();
+const PORT = process.env.APP_PORT || process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Library API listening on ${PORT}`));
